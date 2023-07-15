913cfb3bb2d5abb1c20f70b7b54f02bb89f56612
Tuvshuu <87944616+tuvvshe@users.noreply.github.com>
2023-07-15 12:48:08 +0200

    Add files via upload

---
+++ mad_libs_game.py
@@ -0,0 +1,6 @@
+color = input("farbe eingeben: ")
+pluralNoun = input("Plural Nomen eingeben: ")
+celebrity = input("Eines Celebritys Name eingeben: ")
+print("WIR MOEGEN ", color)
+print(pluralNoun, " is falsch")
+print("Ich mag ", celebrity)
